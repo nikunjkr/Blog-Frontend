b2cb48f9461b6f2242eb557935f863d60d20b4aa
nikunjkr <nikunjkr078@gmail.com>
2020-07-06 16:23:04 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*imorted Route component and BrowserRouter*/
+
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+import HomePage from './pages/HomePage';
+import './App.css';
+import ArticlePage from './pages/ArticlePage';
+import ArticleList from './pages/ArticleList';
+import AboutPage from './pages/AboutPage';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/article" component={ArticlePage} exact/>
+          <Route path="/article-list" component={ArticleList} exact />
+          <Route path="/about" component={AboutPage} exact/>
+           
+        </div >
+      </Router>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1 color="blue">Welcome to our brand New Blogging site.</h1>
+        
+
+        <p1><h2>Three Tips for Working Remotely</h2></p1>
+        <p1>
+            <h3 >1. Create a new norm</h3>
+            <div className="cool-text" >
+            Try to follow your normal work routine to stay productive. If you normally shower every morning or
+            listen to a podcast on your commute, you should still do those things. It sets the right tone for
+            the day (whatever that may be). Also, it’s important to stay flexible, but also maintain boundaries
+            for your working hours. I find it incredibly easy to lose track of time working and, before I know
+            it, it’s 9 p.m.! Set expectations with your manager on when you plan to be online, and whether you
+            need to be available outside your 9-5.
+            </div>
+        </p1>
+
+
+    </>
+)
+
+export default HomePage;
